4618c21a00cd68e86a53b8cfbb8e423f56241a29
Alisson Gois <alisson.go.is@hotmail.com>
2023-12-26 19:30:47 -0300

    Conversor de moeda imersão dev
    conversor de moeda imersão dev com rafa ballerini, Paulo Silveira e outros...

---
+++ Conversor de Moedas Alura/back-end/Converter.js
@@ -0,0 +1,9 @@
+<script>
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.32;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+
+valorEmReal = valorEmReal.toFixed(2);
+alert('R$ '+valorEmReal);
+</script>
