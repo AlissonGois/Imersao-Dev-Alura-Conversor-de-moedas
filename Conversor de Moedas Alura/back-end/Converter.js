790ceeb9af75dac8f362cad2ae33eb40510ad54a
Alisson Gois <alisson.go.is@hotmail.com>
2023-12-27 18:18:40 -0300

    Concertando o front
    Arrumei o front e começar o back-end

--- Conversor de Moedas Alura/back-end/Converter.js
+++
@@ -1,9 +0,0 @@
-<script>
-var valorEmDolar = 30;
-var cotacaoDoDolar = 5.32;
-
-var valorEmReal = valorEmDolar * cotacaoDoDolar;
-
-valorEmReal = valorEmReal.toFixed(2);
-alert('R$ '+valorEmReal);
-</script>
